1cfec208c694176a4069fca465cda3dad0f5e0cb
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-08-22 19:05:14 +0000

    Django

--- checkout/views.py
+++ checkout/views.py
@@ -3,10 +3,11 @@ from django.contrib import messages
 
 from .forms import OrderForm
 
+
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
-        message.error(request, "There's nothing in your bag at the moment")
+        messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
     order_form = OrderForm()
@@ -16,5 +17,3 @@ def checkout(request):
     }
 
     return render(request, template, context)
-
-
